91f440500a95f4a9d98afa9b50db8f877ac5d2ef
Maxim Kuryachiy <maximkuryachiy1998@gmail.com>
2024-11-12 20:39:58 +0200

    Add files via upload

---
+++ Day02/circle.py
@@ -0,0 +1,8 @@
+pi = 3.1415926
+radius = 5
+Area = pi*radius**2
+Circumference = 2*pi*radius
+
+
+print("The area of the circle is:", Area)
+print("The circumference is:", Circumference)
---
+++ Day02/rectangle.py
@@ -0,0 +1,7 @@
+width = 2
+lenght = 3
+Area = width*lenght
+Circumference = 2*(width+lenght)
+
+print("The area of the rectangle is:", Area)
+print("The perimetr is:", Circumference)
