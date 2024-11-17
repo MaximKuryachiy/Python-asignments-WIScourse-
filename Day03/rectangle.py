73f2530d70ee69051821b1b3d802cfcec63c85b1
MaximLaptopPC <maximkuryachiy1998@gmail.com>
2024-11-17 15:47:02 +0200

    Added rectangle.py file with inputs

---
+++ Day03/rectangle.py
@@ -0,0 +1,11 @@
+width = input('Please, enter the width of the rectangle:')
+length = input('Please, enter the lenght:')
+
+width = int(width)
+length = int(length)
+
+Area = width*length
+Circumference = 2*(width+length)
+
+print("The area of the rectangle is:", Area)
+print("The perimetr is:", Circumference)
