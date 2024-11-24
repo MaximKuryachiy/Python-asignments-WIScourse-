5e9e4a5f0e715688559870d0349b106560456eff
MaximLaptopPC <maximkuryachiy1998@gmail.com>
2024-11-24 10:10:50 +0200

    Dilution question added

--- Day03/circle.py
+++ Day03/circle.py
@@ -1,6 +1,7 @@
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--radius", help="the radius of a given circle", type = int, required = True)
+
 args = parser.parse_args()
 
 radius = args.radius
